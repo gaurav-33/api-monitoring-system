a9043d26a39955c1a8e692b9c9281faa2094aee4
gaurav-33 <gauravs.ug23.ec@nitp.ac.in>
2026-03-09 20:58:41 +0530

    add: constant for user roles

---
+++ src/shared/constant/roles.js
@@ -0,0 +1,33 @@
+const ROLES = [
+    'super_admin',
+    'client_admin',
+    'client_viewer'
+]
+
+const CLIENT_ROLES = [
+    'client_admin',
+    'client_viewer'
+]
+
+const APPLICATION_ROLES = {
+    SUPER_ADMIN: 'super_admin',
+    CLIENT_VIEWER: 'client_viewer',
+}
+
+const isValidClientRole = (role) => CLIENT_ROLES.includes(role)
+
+const isValidApplicationRole = (role) => APPLICATION_ROLES.includes(role)
+
+const isValidRole = (role) => ROLES.includes(role)
+
+export {
+    isValidRole,
+    isValidApplicationRole,
+    isValidClientRole
+}
+
+export {
+    ROLES,
+    CLIENT_ROLES,
+    APPLICATION_ROLES
+}
